9f41559535109437b7bff9bf49be61a404196899
Vinicius <8w2jh948cw@privaterelay.appleid.com>
2026-09-03 09:30:05 -0300

    Reverte editor quebrado e restaura Beta 1.2.0-beta.6

--- beta/boot.js
+++ beta/boot.js
@@ -2,7 +2,7 @@
 (() => {
   'use strict';
 
-  const RELEASE = String(window.REGISTRO_SHELL_RELEASE || '1.2.0-beta.7');
+  const RELEASE = String(window.REGISTRO_SHELL_RELEASE || '1.2.0-beta.6');
   const STARTED = performance.now();
   const scopeToken = '/Registro-mental-v1/beta/';
   const hadControllerAtStart = Boolean(navigator.serviceWorker?.controller);
