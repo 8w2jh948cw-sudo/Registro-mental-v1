2e430fbc3faeda37be5f7636a97b8932f70f3461
Vinicius <viniciusnevez123@gmail.com>
2026-09-08 22:16:10 -0300

    Beta 1.2.0-beta.13: atualiza release guard

--- beta/release-guard.js
+++ beta/release-guard.js
@@ -1,2 +1,2 @@
-/* Registro Mental release 1.2.0-beta.12 */
-window.REGISTRO_AVAILABLE_RELEASE='1.2.0-beta.12';
+/* Registro Mental release 1.2.0-beta.13 */
+window.REGISTRO_AVAILABLE_RELEASE='1.2.0-beta.13';
