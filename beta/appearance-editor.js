71ce62c95420782241ce5351be13a94213005774
Vinicius <viniciusnevez123@gmail.com>
2026-09-11 06:13:47 -0300

    Beta 1.2.0-beta.15: sincroniza versão do módulo appearance-editor.js

--- beta/appearance-editor.js
+++ beta/appearance-editor.js
@@ -2,7 +2,7 @@
 (() => {
   'use strict';
 
-  const RELEASE = '1.2.0-beta.14';
+  const RELEASE = '1.2.0-beta.15';
   const SETTINGS_KEY = 'registro-beta-settings-v1';
   const DEFAULT_ICON = {
     home: 'home',
