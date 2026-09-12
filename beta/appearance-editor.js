b1365e8c0793c499b9bf2da81c947f8934684379
Vinicius <viniciusnevez123@gmail.com>
2026-09-12 00:47:10 -0300

    Beta 1.2.0-beta.18: integra seletor Otimizado e Ultra

--- beta/appearance-editor.js
+++ beta/appearance-editor.js
@@ -2,7 +2,7 @@
 (() => {
   'use strict';
 
-  const RELEASE = '1.2.0-beta.17';
+  const RELEASE = '1.2.0-beta.18';
   const SETTINGS_KEY = 'registro-beta-settings-v1';
   const DEFAULT_ICON = {
     home: 'home',
