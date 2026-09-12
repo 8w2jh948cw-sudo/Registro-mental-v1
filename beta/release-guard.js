fb8374ba88341fb26d956f105a55ea72d63f3eb5
Vinicius <viniciusnevez123@gmail.com>
2026-09-12 00:47:16 -0300

    Beta 1.2.0-beta.18: integra seletor Otimizado e Ultra

--- beta/release-guard.js
+++ beta/release-guard.js
@@ -1,2 +1,2 @@
-/* Registro Mental release 1.2.0-beta.17 */
-window.REGISTRO_AVAILABLE_RELEASE='1.2.0-beta.17';
+/* Registro Mental release 1.2.0-beta.18 */
+window.REGISTRO_AVAILABLE_RELEASE='1.2.0-beta.18';
