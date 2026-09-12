76e88c04491859c737d682962d04d0a1fcbde03e
Vinicius <viniciusnevez123@gmail.com>
2026-09-12 00:52:22 -0300

    Beta 1.2.0-beta.19: restaura base visual da Oficial

--- beta/release-guard.js
+++ beta/release-guard.js
@@ -1,2 +1,2 @@
-/* Registro Mental release 1.2.0-beta.18 */
-window.REGISTRO_AVAILABLE_RELEASE='1.2.0-beta.18';
+/* Registro Mental release 1.2.0-beta.19 */
+window.REGISTRO_AVAILABLE_RELEASE='1.2.0-beta.19';
