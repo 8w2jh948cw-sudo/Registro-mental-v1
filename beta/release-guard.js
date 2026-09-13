d5dc3a115943cc6cd5b1fcc12d6f462e09f9f463
Vinicius <viniciusnevez123@gmail.com>
2026-09-13 04:00:38 -0300

    beta: color history filters by area (1.2.0-beta.37)

--- beta/release-guard.js
+++ beta/release-guard.js
@@ -1,2 +1,2 @@
-/* Registro Mental release 1.2.0-beta.36 */
-window.REGISTRO_AVAILABLE_RELEASE='1.2.0-beta.36';
+/* Registro Mental release 1.2.0-beta.37 */
+window.REGISTRO_AVAILABLE_RELEASE='1.2.0-beta.37';
