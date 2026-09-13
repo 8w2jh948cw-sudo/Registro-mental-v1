3827624ea863420f2976bae0de8a589befda91c7
Vinicius <viniciusnevez123@gmail.com>
2026-09-13 04:19:37 -0300

    Beta 40: corrige pluralização de dias

--- beta/release-guard.js
+++ beta/release-guard.js
@@ -1,2 +1,2 @@
-/* Registro Mental release 1.2.0-beta.39 */
-window.REGISTRO_AVAILABLE_RELEASE='1.2.0-beta.39';
+/* Registro Mental release 1.2.0-beta.40 */
+window.REGISTRO_AVAILABLE_RELEASE='1.2.0-beta.40';
