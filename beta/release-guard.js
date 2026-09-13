6ecc34cf1150cba9d6d5be793e09e0488a1c1fa3
Vinicius <viniciusnevez123@gmail.com>
2026-09-13 02:03:15 -0300

    beta: align purchase location and replace icon (1.2.0-beta.28)

--- beta/release-guard.js
+++ beta/release-guard.js
@@ -1,2 +1,2 @@
-/* Registro Mental release 1.2.0-beta.27 */
-window.REGISTRO_AVAILABLE_RELEASE='1.2.0-beta.27';
+/* Registro Mental release 1.2.0-beta.28 */
+window.REGISTRO_AVAILABLE_RELEASE='1.2.0-beta.28';
