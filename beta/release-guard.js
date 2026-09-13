5e9e7b511709da12d265496f8ff57eb6fc0a359c
Vinicius <viniciusnevez123@gmail.com>
2026-09-13 03:24:44 -0300

    beta: color purchase location icon with card theme (1.2.0-beta.29)

--- beta/release-guard.js
+++ beta/release-guard.js
@@ -1,2 +1,2 @@
-/* Registro Mental release 1.2.0-beta.28 */
-window.REGISTRO_AVAILABLE_RELEASE='1.2.0-beta.28';
+/* Registro Mental release 1.2.0-beta.29 */
+window.REGISTRO_AVAILABLE_RELEASE='1.2.0-beta.29';
