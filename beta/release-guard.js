11da1e29bf0aa8849d7b43fdff5907619776b435
Vinicius <viniciusnevez123@gmail.com>
2026-09-13 01:16:47 -0300

    beta: align startup and cache release 1.2.0-beta.25

--- beta/release-guard.js
+++ beta/release-guard.js
@@ -1,2 +1,2 @@
-/* Registro Mental release 1.2.0-beta.19 */
-window.REGISTRO_AVAILABLE_RELEASE='1.2.0-beta.19';
+/* Registro Mental release 1.2.0-beta.25 */
+window.REGISTRO_AVAILABLE_RELEASE='1.2.0-beta.25';
